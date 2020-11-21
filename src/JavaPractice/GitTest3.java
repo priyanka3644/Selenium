e2935be3578d5d3e1bd3e718d07d3f7ad42aa58f
priya <priyankasanka@gmail.com>
2020-11-21 13:11:39 +0530

    branch Commit

---
+++ src/JavaPractice/GitTest3.java
@@ -0,0 +1,12 @@
+package JavaPractice;
+
+public class GitTest3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("created a new files to test GIT Branching");
+
+	}
+
+}
